fc184f9817ddeddcf0251eff69debc7083fd558e
psndna88 <psndna88@gmail.com>
2016-11-19 12:56:07 +0530

    ASoC: wcd9335: Sound control (set default gain to 0)

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -13775,7 +13775,8 @@ static ssize_t headphone_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
 
-	int input_l, input_r;
+	int input_l = 0;
+	int input_r = 0;
 
 	sscanf(buf, "%d %d", &input_l, &input_r);
 
@@ -13811,7 +13812,8 @@ static ssize_t headphone_pa_gain_show(struct kobject *kobj,
 static ssize_t headphone_pa_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	unsigned int input_l, input_r;
+	unsigned int input_l = 0;
+	unsigned int input_r = 0;
 	struct tasha_priv *tasha = snd_soc_codec_get_drvdata(sound_control_codec_ptr);
 
 	sscanf(buf, "%d %d", &input_l, &input_r);
@@ -13847,11 +13849,11 @@ static ssize_t mic_gain_show(struct kobject *kobj,
 static ssize_t mic_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int input;
+	int input = 0;
 
 	sscanf(buf, "%d", &input);
 
-	if (input < -10 || input > 20)
+	if (input < -10 || input > 10)
 		input = 0;
 
 	snd_soc_write(sound_control_codec_ptr, WCD9335_CDC_RX0_RX_VOL_CTL, input);
@@ -13874,11 +13876,11 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 static ssize_t speaker_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int input;
+	int input = 0;
 
 	sscanf(buf, "%d", &input);
 
-	if (input < -10 || input > 20)
+	if (input < -10 || input > 10)
 		input = 0;
 
 	snd_soc_write(sound_control_codec_ptr, WCD9335_CDC_RX6_RX_VOL_CTL, input);
