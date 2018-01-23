c627df70c988c6ed8a0bcf324e5314881a86e21f
LuanHalaiko <tecnotailsplays@gmail.com>
2018-01-23 10:27:38 -0200

    Cpufreq: Remove useless entries

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -849,9 +849,7 @@ cpufreq_freq_attr_rw(scaling_min_freq);
 cpufreq_freq_attr_rw(scaling_max_freq);
 cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
-/* SysFS Undervolts Control */
 cpufreq_freq_attr_rw(UV_mV_table);
-/* SysFS Undervolts Control */
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -865,9 +863,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_driver.attr,
 	&scaling_available_governors.attr,
 	&scaling_setspeed.attr,
-	/* SysFS Undervolts Control */
 	&UV_mV_table.attr,
-	/* SysFS Undervolts Control */
 	NULL
 };
 
