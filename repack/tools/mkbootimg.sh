c34da86b7b7a20d6e839caeb506d14f433fea436
LuanHalaiko <tecnotailsplays@gmail.com>
2018-01-25 00:21:43 -0200

    repack: mkbootimg.sh: update os level patch

--- repack/tools/mkbootimg.sh
+++ repack/tools/mkbootimg.sh
@@ -28,7 +28,7 @@ fi;
 if [ -f /tmp/boot.img-oslevel ]; then
 	osvel=`cat /tmp/boot.img-oslevel`;
 else
-	osvel=2017-11;
+	osvel=2018-01;
 fi;
 
 /tmp/mkbootimg --kernel /tmp/zImage --ramdisk /tmp/boot.img-ramdisk.gz --cmdline "$cmdline" --board "$board" --base $base --pagesize $pagesize --kernel_offset $kerneloff --ramdisk_offset $ramdiskoff --second_offset $secondoff --tags_offset "$tagsoff" --os_version "$osver" --os_patch_level "$osvel" --hash "$hashfck" --output /tmp/newboot.img;
